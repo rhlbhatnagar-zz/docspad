f90d9164ab6abac1a929ef8f2a6ab2bc435ac4ad
rhlbhatnagar <rahulbh@10ddb1d562a1.ant.amazon.com>
2013-09-15 09:38:48 +0530

    Check for larger files

--- php/PhpClient.php
+++ php/PhpClient.php
@@ -1,81 +1,81 @@
 <?php
-
-namespace docspad\client\php;
-
-error_reporting(E_ALL);
-
-require_once __DIR__.'/../../lib/php/lib/Thrift/ClassLoader/ThriftClassLoader.php';
-
-use Thrift\ClassLoader\ThriftClassLoader;
-
-$GEN_DIR = realpath(dirname(__FILE__).'/..').'/gen-php';
-
-$loader = new ThriftClassLoader();
-$loader->registerNamespace('Thrift', __DIR__ . '/../lib/php/lib');
-$loader->registerDefinition('docspad\client', $GEN_DIR);
-$loader->register();
-
-use Thrift\Protocol\TBinaryProtocol;
-use Thrift\Transport\TSocket;
-use Thrift\Transport\THttpClient;
-use Thrift\Transport\TBufferedTransport;
-use Thrift\Exception\TException;
-
-class DocspadClient{
     
-    private $client;
-    private $transport;
+    namespace docspad\client\php;
     
-    public function __construct(){
-        echo 'entered constructor...';
-
-        $socket = new THttpClient('localhost', 8080, '/php/PhpServer.php');
-        $this->transport = new TBufferedTransport($socket, 1024, 1024);
-        $protocol = new TBinaryProtocol($this->transport);
-        $this->client = new \docspad\client\DocspadClient($protocol);
-        echo 'left constructor...';
-
+    error_reporting(E_ALL);
     
-    }
+    require_once __DIR__.'/../../lib/php/lib/Thrift/ClassLoader/ThriftClassLoader.php';
     
-    public function __destruct(){
-        $this->transport->close();
-    }
+    use Thrift\ClassLoader\ThriftClassLoader;
     
-    public function upload($filename){
-        $file = __DIR__ .'/client-dir/'.$filename;
-        $io = new \docspad\client\InvalidOperation();
-        if(!file_exists($file)){
-            $io->what =\docspad\client\DocspadOperation::UPLOAD;
-            $io->why = "The given file does not exist on your client";
-            throw $io;
+    $GEN_DIR = realpath(dirname(__FILE__).'/..').'/gen-php';
+    
+    $loader = new ThriftClassLoader();
+    $loader->registerNamespace('Thrift', __DIR__ . '/../lib/php/lib');
+    $loader->registerDefinition('docspad\client', $GEN_DIR);
+    $loader->register();
+    
+    use Thrift\Protocol\TBinaryProtocol;
+    use Thrift\Transport\TSocket;
+    use Thrift\Transport\THttpClient;
+    use Thrift\Transport\TBufferedTransport;
+    use Thrift\Exception\TException;
+    
+    class DocspadClient{
+        
+        private $client;
+        private $transport;
+        
+        public function __construct(){
+            echo 'entered constructor...';
+            
+            $socket = new THttpClient('localhost', 8080, '/php/PhpServer.php');
+            $this->transport = new TBufferedTransport($socket, 1024, 1024);
+            $protocol = new TBinaryProtocol($this->transport);
+            $this->client = new \docspad\client\DocspadClient($protocol);
+            echo 'left constructor...';
+            
+            
         }
-        $contents = file_get_contents($file);
         
-        $doc = new \docspad\client\Document();
-        $doc->filename = $filename;
-        $doc->contents= $contents;
+        public function __destruct(){
+            $this->transport->close();
+        }
         
-        return $this->client->upload($doc);
+        public function upload($filename){
+            $file = __DIR__ .'/client-dir/'.$filename;
+            $io = new \docspad\client\InvalidOperation();
+            if(!file_exists($file)){
+                $io->what =\docspad\client\DocspadOperation::UPLOAD;
+                $io->why = "The given file does not exist on your client";
+                throw $io;
+            }
+            $contents = file_get_contents($file);
+            
+            $doc = new \docspad\client\Document();
+            $doc->filename = $filename;
+            $doc->contents= $contents;
+            
+            return $this->client->upload($doc);
+        }
+        
+        public function download($filename){
+            $file = __DIR__ .'/client-dir/'.$filename;
+            $contents = $this->client->download($filename);
+            file_put_contents($file, $contents);
+            return 1;
+        }
+        
     }
     
-    public function download($filename){
-        $file = __DIR__ .'/client-dir/'.$filename;
-        $contents = $this->client->download($filename);
-        file_put_contents($file, $contents);
-        return 1;
+    try {
+        
+        $PhpClient = new DocspadClient();
+        // $PhpClient->upload("people.txt");
+        $PhpClient->download("something.jpg");
+        
+    } catch (TException $tx) {
+        print 'TException: '.$tx->why."\n";
     }
-
-}
     
-try {
-
-    $PhpClient = new DocspadClient();
-   // $PhpClient->upload("people.txt");
-    $PhpClient->download("people.txt");
-    
-} catch (TException $tx) {
-  print 'TException: '.$tx->why."\n";
-}
-
-?>
+    ?>
